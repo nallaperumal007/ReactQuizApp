f11c5d16c77b436d38d445962bc1908bad826803
nallaperumal007 <nallaperumal342@gmail.com>
2023-07-27 12:15:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './Header';
+export default function App() {
+	const questions = [
+		{
+			questionText: 'What is ReactJS?',
+			answerOptions: [
+				{ answerText: 'Serverside Framework', isCorrect: false },
+				{ answerText: 'User Interface Framework', isCorrect: true },
+				{ answerText: 'back end framework', isCorrect: false},
+				{ answerText: 'none', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who created React.js?  ',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Jordan walke', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		},
+		{
+			questionText: 'In which language is React.js written?      ',
+			answerOptions: [
+				{ answerText: 'Javascript', isCorrect: true },
+				{ answerText: 'Java', isCorrect: false },
+				{ answerText: 'Python', isCorrect: false },
+				{ answerText: 'C#', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which of the following is used in React.js to increase performance?',
+			answerOptions: [
+				{ answerText: 'Orginal Dom', isCorrect: false },
+				{ answerText: 'Bom', isCorrect: false },
+				{ answerText: 'None', isCorrect: false },
+				{ answerText: 'Virtual Dom', isCorrect: true },
+			],
+		},
+	];
+  
+
+  const[currentQuestion,setCurrentQuestion]=useState(0);
+  const[showScore,setShowScore]=useState(false);
+  const[score,setScore]=useState(0);
+
+  const handleAnswerOptionClick=(isCorrect)=>{
+    if(isCorrect)
+    {
+      setScore(score+1);
+    }
+    const nextQuestion=currentQuestion+1;
+    if(nextQuestion<questions.length){
+       setCurrentQuestion(nextQuestion);
+    }
+    else{
+      setShowScore(true);
+    }
+  };
+
+
+	return (
+		<div className='app'>
+			{/* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+			{showScore ? (
+				<div className='score-section'>You scored {score} out of {questions.length}</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question{currentQuestion+1} </span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOptions)=>
+            <button onClick={()=>handleAnswerOptionClick(answerOptions.isCorrect)}>
+             {answerOptions.answerText}</button>
+            )}
+
+          
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+export const Header = () => {
+  return (
+    <div>
+       <h3>React Quiz App</h3>
+       <h4>I am Nalla Perumal Mern Stack Developer</h4>
+    </div>
+  )
+}
